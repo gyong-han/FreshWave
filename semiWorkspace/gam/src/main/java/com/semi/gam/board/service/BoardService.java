3d54645c19072c68c9440bad037c43e7b857aa2e
DESKTOP-PH5JOLL\user1 <abc@yaho.com>
2024-11-25 19:05:53 +0900

    Board 기본 커밋

--- semiWorkspace/gam/src/main/java/com/semi/gam/board/service/BoardService.java
+++ semiWorkspace/gam/src/main/java/com/semi/gam/board/service/BoardService.java
@@ -1,4 +1,27 @@
 package com.semi.gam.board.service;
 
+import com.semi.gam.board.mapper.BoardMapper;
+import com.semi.gam.board.vo.BoardVo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class BoardService {
+
+    private final BoardMapper mapper;
+
+    public List<BoardVo> getBoardList() {
+        return mapper.getBoardList();
+    }
+
+    public int write(BoardVo vo) {
+        return mapper.write(vo);
+    }
+
+    public BoardVo getBoardDetail(String bno) {
+        return mapper.getBoard(bno);
+    }
 }
