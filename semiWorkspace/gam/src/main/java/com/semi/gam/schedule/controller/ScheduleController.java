63f5326f4111afe5de11a74f4c872580b6b4e5bf
dldprma <amrpdld@gmail.com>
2024-11-26 11:24:42 +0900

    1125 작업내역(헤더수정, 폰트추가)

--- semiWorkspace/gam/src/main/java/com/semi/gam/schedule/controller/ScheduleController.java
+++ semiWorkspace/gam/src/main/java/com/semi/gam/schedule/controller/ScheduleController.java
@@ -1,4 +1,31 @@
 package com.semi.gam.schedule.controller;
 
+import com.semi.gam.schedule.service.ScheduleService;
+import com.semi.gam.schedule.vo.ScheduleVo;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("sch")
+@Slf4j
+@RequiredArgsConstructor
 public class ScheduleController {
+
+    private final ScheduleService ss;
+
+    @GetMapping("write")
+    public String write(){
+        return "write";
+    }
+
+    @PostMapping("write")
+    public void write(ScheduleVo vo){
+        ss.write(vo);
+
+        return "";
+    }
 }
--- semiWorkspace/gam/src/main/java/com/semi/gam/schedule/vo/ScheduleVo.java
+++ semiWorkspace/gam/src/main/java/com/semi/gam/schedule/vo/ScheduleVo.java
@@ -1,4 +1,24 @@
 package com.semi.gam.schedule.vo;
 
+import lombok.Data;
+
+@Data
 public class ScheduleVo {
+    private String no;
+    private String writerNo;
+    private String memoNo;
+    private String bussinessNo;
+    private String busihisNo;
+    private String storeNo;
+    private String priority;
+    private String title;
+    private String content;
+    private String location;
+    private String startDate;
+    private String finishDate;
+    private String startTime;
+    private String finishTime;
+    private String almTime;
+    private String shareYn;
+    private String delYn;
 }
