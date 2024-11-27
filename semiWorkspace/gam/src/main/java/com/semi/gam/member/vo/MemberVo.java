07d206af428ad2ab5b6abd0293e27d0e4c86bb30
LAPTOP-J9C5C5F4\samsung <aco2829@naver.com>
2024-11-27 20:19:59 +0900

    1127 member,admin was+jsp / dept,job,employee vo생성

--- semiWorkspace/gam/src/main/java/com/semi/gam/member/vo/MemberVo.java
+++ semiWorkspace/gam/src/main/java/com/semi/gam/member/vo/MemberVo.java
@@ -1,4 +1,19 @@
 package com.semi.gam.member.vo;
 
+import lombok.Data;
+
+@Data
 public class MemberVo {
+    private String no;
+    private String id;
+    private String cpCode;
+    private String name;
+    private String pwd;
+    private String nick;
+    private String idNum;
+    private String gender;
+    private String phone;
+    private String email;
+    private String address;
+    private String profile;
 }
